c64ab0d9411fcbe45aaa3b2245dd7cda870a131a
MayzaHirose <maayhirose@gmail.com>
2016-12-14 18:39:16 -0200

    Cadastro de filmes adicionado
    cadastro de filmes e correta inserção na lista por ordem alfabética do
    título

--- trab-plm-gnu-assembler.s
+++ trab-plm-gnu-assembler.s
@@ -1,39 +1,59 @@
-#Mayza Yuri Hirose da Costa
+#Mayza Yuri Hirose da Costa RA88739
 
 .section .data
 
+#STRINGS MENU
 menu:		.asciz	"\n*****************************\n* Gerenciamento de Locadora *\n*****************************\n"
-menuopcoes:		.asciz	"\nEscolha uma opcao:\n<1>Cliente\n<2>Filmes\n<3>Locação\n<4>Sair\n\n"
+menuopcoes:	.asciz	"\nEscolha uma opcao:\n<1>Cliente\n<2>Filmes\n<3>Locação\n<4>Sair\n\n"
 submenu:	.asciz	"\n*****************\n* O que deseja? *\n*****************\n"
 submenuopcoes:	.asciz	"\n<1>Cadastrar\n<2>Consultar\n<3>Relatório\n<4>Voltar\n\n"
 menulocacao:	.asciz	"\n******* EM CONSTRUÇÃO *******\n\n"
 
 #CADASTRO DE CLIENTE
-pedenome:	.asciz	"\n>Dados Pessoais<\nNome:"
-pedecpf:	.asciz	"CPF:"
-pederg:		.asciz	"RG:"
-pedeemail:	.asciz	"Email:"
-pederua:	.asciz	"\n>Endereço<\nRua:"
-pedenum:	.asciz	"Nº:"
-pedebairro:	.asciz	"Bairro:"
-pedecidade:	.asciz	"Cidade:"
-pedetelefone:	.asciz	"Telefone:"
+nome:		.asciz	"\n>Dados Pessoais<\nNome:"
+cpf:		.asciz	"CPF:"
+rg:		.asciz	"RG:"
+email:		.asciz	"Email:"
+rua:		.asciz	"\n>Endereço<\nRua:"
+num:		.asciz	"Nº:"
+bairro:		.asciz	"Bairro:"
+cidade:		.asciz	"Cidade:"
+telefone:	.asciz	"Telefone:"
+
+#CADASTRO DE FILME
+titulo:		.asciz	"\nTitulo:"
+anolancamento:	.asciz	"Ano de Lançamento:"
+duracao:	.asciz	"Duração:"
+atorprincipal:	.asciz	"Ator Principal:"
+diretor:	.asciz	"Diretor:"
+totalcopias:	.asciz	"Total de Copias:"
+locadas:	.asciz	"Copias Locadas:"
+disponiveis:	.asciz	"Copias disponiveis:"
+totallocacoes:	.asciz	"Total de Locacoes:"
+avmedia:	.asciz	"Nota dos Clientes:"
+
+#STRINGS AUXILIARES
 pl:		.asciz	"\n"
 pl2:		.asciz	"--------------------------"
 
-#RELATORIO DE CLIENTES
-enfeiterelatorio:	.asciz	"\n************************\n* Clientes Cadastrados *\n************************\n"
-finallistaclientes:	.asciz	"\n\n***** FIM DA LISTA *****\n"
+#STRINGS RELATORIOS
+enfrelatoriocliente:	.asciz	"\n************************\n* Clientes Cadastrados *\n************************\n"
+enfrelatoriofilme:		.asciz	"\n************************\n* Filmes Cadastrados *\n************************\n"
+finallista:		.asciz	"\n\n***** FIM DA LISTA *****\n"
 
-#Registro do cliente = Nome(40) CPF(20) RG(20) Email(30) Rua(40) Nº(10) Bairro(25) Cidade(20) Telefone(20) Assistidos(4) Locados(4) Reserva(4) Saldo(4) prox(4) = 245
+#Registro do Cliente = Nome(40) CPF(20) RG(20) Email(30) Rua(40) Nº(10) Bairro(25) Cidade(20) Telefone(20) Assistidos(4) Locados(4) Reserva(4) Saldo(4) prox(4) = 245
+
+#Registro do Filme = Titulo(40) Ano Lançamento(15) Duração(15) Ator Principal(20) Diretor(20) Total Copias(5) Locadas(5) Disponiveis(5) Num Locaçoes(5) Av Media(5) Clientes que Locaram(4) clientes na Espera(4) prox(4) = 147
 
 opcao:		.int	0
-formato:	.asciz	"%d"
-NULL:		.int	0
-listaclientes:	.int 	NULL
-tamregcliente:	.int	245
-
+formatodec:	.asciz	"%d"
 limpabuf:	.asciz	"%c"
+NULL:		.int	0
+
+listaclientes:	.int 	NULL
+listafilmes:	.int	NULL
+tamregcliente:	.int	245
+tamregfilme:	.int	147
 
 .section .text
 
@@ -53,7 +73,7 @@ telainicial:
 	call	printf
 
 	pushl	$opcao
-	pushl	$formato
+	pushl	$formatodec
 	call	scanf
 
 	addl	$12, %esp
@@ -85,7 +105,7 @@ opcaocliente:
 	call	printf
 
 	pushl	$opcao
-	pushl	$formato
+	pushl	$formatodec
 	call	scanf
 
 	addl	$16, %esp
@@ -119,10 +139,10 @@ cadastrocliente:
 	cmpl	$NULL, %esi
 	je	insereprimeirocliente
 
-	call	encaixaclientenalista
+	call	achaposicaonalistaclientes
 
 	cmpl	$NULL, %esi
-	je	inserecliini
+	je	insereregistroiniciocliente
 	movl	241(%esi), %ecx
 	movl	%ecx, 241(%edi)
 	movl	%edi, 241(%esi)
@@ -137,7 +157,7 @@ pegadadoscliente:
 	call	scanf
 	addl	$4, %esp
 
-	pushl	$pedenome
+	pushl	$nome
 	call	printf
 	addl	$4, %esp
 	call	gets
@@ -145,7 +165,7 @@ pegadadoscliente:
 	addl	$40, %edi
 	pushl	%edi
 
-	pushl	$pedecpf
+	pushl	$cpf
 	call	printf
 	addl	$4, %esp
 	call	gets
@@ -153,7 +173,7 @@ pegadadoscliente:
 	addl	$20, %edi
 	pushl	%edi
 
-	pushl	$pederg
+	pushl	$rg
 	call	printf
 	addl	$4, %esp
 	call	gets
@@ -161,7 +181,7 @@ pegadadoscliente:
 	addl	$20, %edi
 	pushl	%edi
 
-	pushl	$pedeemail
+	pushl	$email
 	call	printf
 	addl	$4, %esp
 	call	gets
@@ -169,7 +189,7 @@ pegadadoscliente:
 	addl	$30, %edi
 	pushl	%edi
 
-	pushl	$pederua
+	pushl	$rua
 	call	printf
 	addl	$4, %esp
 	call	gets
@@ -177,7 +197,7 @@ pegadadoscliente:
 	addl	$40, %edi
 	pushl	%edi
 
-	pushl	$pedenum
+	pushl	$num
 	call	printf
 	addl	$4, %esp
 	call	gets
@@ -185,7 +205,7 @@ pegadadoscliente:
 	addl	$10, %edi
 	pushl	%edi
 
-	pushl	$pedebairro
+	pushl	$bairro
 	call	printf
 	addl	$4, %esp
 	call	gets
@@ -193,7 +213,7 @@ pegadadoscliente:
 	addl	$25, %edi
 	pushl	%edi
 
-	pushl	$pedecidade
+	pushl	$cidade
 	call	printf
 	addl	$4, %esp
 	call	gets
@@ -201,7 +221,7 @@ pegadadoscliente:
 	addl	$20, %edi
 	pushl	%edi
 
-	pushl	$pedetelefone
+	pushl	$telefone
 	call	printf
 	addl	$4, %esp
 	call	gets
@@ -233,7 +253,7 @@ insereprimeirocliente:
 
 	ret
 
-encaixaclientenalista:
+achaposicaonalistaclientes:
 	
 	movl	listaclientes, %esi
 	pushl	%edi
@@ -242,12 +262,12 @@ encaixaclientenalista:
 	popl	%esi
 	popl	%edi
 
-	jl	contcli
+	jl	procuraposicaomeioclientes
 	movl	$NULL, %esi
 	
 	ret
 
-inserecliini:
+insereregistroiniciocliente:
 
 	movl	listaclientes, %esi
 	movl	%esi, 241(%edi) #passa o endereço apontado pela lista para o final do registro a ser adicionado. 
@@ -255,41 +275,36 @@ inserecliini:
 
 	ret
 
-contcli:	
+procuraposicaomeioclientes:	
 	
 	movl	241(%esi), %ecx
 	cmpl	$NULL, %ecx
-	jne	avancacli
+	jne	existeregistroaindaclientes
 
 	ret
 
-avancacli:
+existeregistroaindaclientes:
 	
 	pushl	%esi
 	movl	%ecx, %esi
-	jmp	voltacli
 
-	ret
-
-voltacli:
-	
+	#verifica se o proximo reg é maior ou menor
 	pushl	%edi
 	pushl	%esi
 	call	strcmp
 	popl	%esi
 	popl	%edi
 
-	jl	contcliaux
-	#movl	%esp, %esi 
-	popl	%esi	#ok, caso em que o B É MAIOR QUE O Q ESTA SENDO INSERIDO 
+	jl	registroemaiorcliente
+	popl	%esi	#se o registro é menor, entao recupera o %esi da pilha
 
 	ret
 
-contcliaux:
+registroemaiorcliente:
 
 	addl	$4, %esp
 	#movl	%ecx, %esi
-	jmp	contcli
+	jmp	procuraposicaomeioclientes
 
 	ret
 
@@ -299,30 +314,30 @@ consultarcliente:
 
 relatoriocliente:
 	
-	pushl	$enfeiterelatorio
+	pushl	$enfrelatoriocliente
 	call	printf
 	addl	$4, %esp
 
 	movl	listaclientes, %edi
 		
-	call	verificalista
+	call	verificalistaclientes
 
 	ret
 
-verificalista:
+verificalistaclientes:
 
 	cmpl	$NULL, %edi
 	je	fimlista
 
 	call	mostracliente
 	movl	241(%edi), %edi #pega o valor apontado pela posicao 241 do %edi e passa pro %edi
-	jmp	verificalista
+	jmp	verificalistaclientes
 
 	ret
 
 mostracliente:
 
-	pushl	$pedenome
+	pushl	$nome
 	call	printf
 	pushl	%edi
 	call	printf
@@ -332,7 +347,7 @@ mostracliente:
 	call	printf
 	addl	$4, %esp
 
-	pushl	$pedecpf
+	pushl	$cpf
 	call	printf
 	pushl	%edi
 	call	printf
@@ -342,7 +357,7 @@ mostracliente:
 	call	printf
 	addl	$4, %esp
 
-	pushl	$pederg
+	pushl	$rg
 	call	printf
 	pushl	%edi
 	call	printf
@@ -352,7 +367,7 @@ mostracliente:
 	call	printf
 	addl	$4, %esp
 
-	pushl	$pedeemail
+	pushl	$email
 	call	printf
 	pushl	%edi
 	call	printf
@@ -362,7 +377,7 @@ mostracliente:
 	call	printf
 	addl	$4, %esp
 
-	pushl	$pederua
+	pushl	$rua
 	call	printf
 	pushl	%edi
 	call	printf
@@ -372,7 +387,7 @@ mostracliente:
 	call	printf
 	addl	$4, %esp
 
-	pushl	$pedenum
+	pushl	$num
 	call	printf
 	pushl	%edi
 	call	printf
@@ -382,7 +397,7 @@ mostracliente:
 	call	printf
 	addl	$4, %esp
 
-	pushl	$pedebairro
+	pushl	$bairro
 	call	printf
 	pushl	%edi
 	call	printf
@@ -392,7 +407,7 @@ mostracliente:
 	call	printf
 	addl	$4, %esp
 
-	pushl	$pedecidade
+	pushl	$cidade
 	call	printf
 	pushl	%edi
 	call	printf
@@ -402,7 +417,7 @@ mostracliente:
 	call	printf
 	addl	$4, %esp
 
-	pushl	$pedetelefone
+	pushl	$telefone
 	call	printf
 	pushl	%edi
 	call	printf
@@ -422,7 +437,7 @@ mostracliente:
 
 fimlista:
 
-	pushl	$finallistaclientes
+	pushl	$finallista
 	call	printf
 	addl	$4, %esp
 	
@@ -434,9 +449,9 @@ opcaofilme:
 	call	printf
 	pushl	$submenuopcoes
 	call	printf
-	
+
 	pushl	$opcao
-	pushl	$formato
+	pushl	$formatodec
 	call	scanf
 
 	addl	$16, %esp
@@ -457,19 +472,187 @@ opcaofilme:
 
 	ret
 
-opcaolocacao:
-
-	pushl	$menulocacao
-	call	printf
-	addl	$4, %esp
-
-	ret
-
 cadastrofilme:
+	
+	pushl	$tamregfilme
+	call	malloc
+	movl	%eax, %edi
+	addl	$4, %esp
+	
+	call	pegadadosfilme
+	movl	listafilmes, %esi
+
+	cmpl	$NULL, %esi
+	je	insereprimeirofilme
+
+	call	achaposicaonalistafilmes
+
+	cmpl	$NULL, %esi
+	je	insereregistroiniciofilme
+	movl	143(%esi), %ecx
+	movl	%ecx, 143(%edi)
+	movl	%edi, 143(%esi)
 	
 	ret
 
 pegadadosfilme:
+
+	pushl	%edi
+	
+	pushl	$limpabuf
+	call	scanf
+	addl	$4, %esp
+
+	pushl	$titulo
+	call	printf
+	addl	$4, %esp
+	call	gets
+	popl	%edi
+	addl	$40, %edi
+	pushl	%edi
+
+	pushl	$anolancamento
+	call	printf
+	addl	$4, %esp
+	call	gets
+	popl	%edi
+	addl	$15, %edi
+	pushl	%edi
+
+	pushl	$duracao
+	call	printf
+	addl	$4, %esp
+	call	gets
+	popl	%edi
+	addl	$15, %edi
+	pushl	%edi
+
+	pushl	$atorprincipal
+	call	printf
+	addl	$4, %esp
+	call	gets
+	popl	%edi
+	addl	$20, %edi
+	pushl	%edi
+
+	pushl	$diretor
+	call	printf
+	addl	$4, %esp
+	call	gets
+	popl	%edi
+	addl	$20, %edi
+	pushl	%edi
+
+	pushl	$totalcopias
+	call	printf
+	addl	$4, %esp
+	call	gets
+	popl	%edi
+	addl	$5, %edi
+	pushl	%edi
+
+	pushl	$locadas
+	call	printf
+	addl	$4, %esp
+	call	gets
+	popl	%edi
+	addl	$5, %edi
+	pushl	%edi
+
+	pushl	$disponiveis
+	call	printf
+	addl	$4, %esp
+	call	gets
+	popl	%edi
+	addl	$5, %edi
+	pushl	%edi
+
+	pushl	$totallocacoes
+	call	printf
+	addl	$4, %esp
+	call	gets
+	popl	%edi
+	addl	$5, %edi
+	pushl	%edi
+
+	pushl	$avmedia
+	call	printf
+	addl	$4, %esp
+	call	gets
+	popl	%edi
+	addl	$5, %edi
+
+	movl	$NULL, (%edi);
+	addl	$4, %edi
+
+	movl	$NULL, (%edi);
+	addl	$4, %edi
+
+	movl	$NULL, (%edi);
+	#addl	$4, %edi
+
+	subl	$143, %edi
+
+	ret
+
+insereprimeirofilme:
+
+	movl	%edi, listafilmes
+
+	ret
+
+achaposicaonalistafilmes:
+	
+	movl	listafilmes, %esi
+	pushl	%edi
+	pushl	%esi
+	call	strcmp
+	popl	%esi
+	popl	%edi
+
+	jl	procuraposicaomeiofilmes
+	movl	$NULL, %esi
+	
+	ret
+
+insereregistroiniciofilme:
+
+	movl	listafilmes, %esi
+	movl	%esi, 143(%edi) #passa o endereço apontado pela lista para o final do registro a ser adicionado. 
+	movl	%edi, listafilmes
+
+	ret
+
+procuraposicaomeiofilmes:	
+	
+	movl	143(%esi), %ecx
+	cmpl	$NULL, %ecx
+	jne	existeregistroaindafilmes
+
+	ret
+
+existeregistroaindafilmes:
+	
+	pushl	%esi
+	movl	%ecx, %esi
+
+	#verifica se o proximo reg é maior ou menor
+	pushl	%edi
+	pushl	%esi
+	call	strcmp
+	popl	%esi
+	popl	%edi
+
+	jl	registroemaiorfilme
+	popl	%esi	#se o registro é menor, entao recupera o %esi da pilha
+
+	ret
+
+registroemaiorfilme:
+
+	addl	$4, %esp
+	#movl	%ecx, %esi
+	jmp	procuraposicaomeiofilmes
 
 	ret
 
@@ -479,6 +662,143 @@ consultarfilme:
 
 relatoriofilme:
 	
+	pushl	$enfrelatoriofilme
+	call	printf
+	addl	$4, %esp
+
+	movl	listafilmes, %edi
+		
+	call	verificalistafilmes
+
+	ret
+
+verificalistafilmes:
+
+	cmpl	$NULL, %edi
+	je	fimlista
+
+	call	mostrafilme
+	movl	143(%edi), %edi #pega o valor apontado pela posicao 143 do %edi e passa pro %edi
+	jmp	verificalistafilmes
+
+	ret
+
+mostrafilme:
+
+	pushl	$titulo
+	call	printf
+	pushl	%edi
+	call	printf
+	addl	$8, %esp
+	addl	$40, %edi
+	pushl	$pl
+	call	printf
+	addl	$4, %esp
+
+	pushl	$anolancamento
+	call	printf
+	pushl	%edi
+	call	printf
+	addl	$8, %esp
+	addl	$15, %edi
+	pushl	$pl
+	call	printf
+	addl	$4, %esp
+
+	pushl	$duracao
+	call	printf
+	pushl	%edi
+	call	printf
+	addl	$8, %esp
+	addl	$15, %edi
+	pushl	$pl
+	call	printf
+	addl	$4, %esp
+
+	pushl	$atorprincipal
+	call	printf
+	pushl	%edi
+	call	printf
+	addl	$8, %esp
+	addl	$20, %edi
+	pushl	$pl
+	call	printf
+	addl	$4, %esp
+
+	pushl	$diretor
+	call	printf
+	pushl	%edi
+	call	printf
+	addl	$8, %esp
+	addl	$20, %edi
+	pushl	$pl
+	call	printf
+	addl	$4, %esp
+
+	pushl	$totalcopias
+	call	printf
+	pushl	%edi
+	call	printf
+	addl	$8, %esp
+	addl	$5, %edi
+	pushl	$pl
+	call	printf
+	addl	$4, %esp
+
+	pushl	$locadas
+	call	printf
+	pushl	%edi
+	call	printf
+	addl	$8, %esp
+	addl	$5, %edi
+	pushl	$pl
+	call	printf
+	addl	$4, %esp
+
+	pushl	$disponiveis
+	call	printf
+	pushl	%edi
+	call	printf
+	addl	$8, %esp
+	addl	$5, %edi
+	pushl	$pl
+	call	printf
+	addl	$4, %esp
+
+	pushl	$totallocacoes
+	call	printf
+	pushl	%edi
+	call	printf
+	addl	$8, %esp
+	addl	$5, %edi
+	pushl	$pl
+	call	printf
+	addl	$4, %esp
+
+	pushl	$avmedia
+	call	printf
+	pushl	%edi
+	call	printf
+	addl	$8, %esp
+	addl	$5, %edi
+	pushl	$pl
+	call	printf
+	addl	$4, %esp
+
+	pushl	$pl2
+	call	printf
+	addl	$4, %esp
+
+	subl	$135, %edi
+
+	ret
+
+opcaolocacao:
+
+	pushl	$menulocacao
+	call	printf
+	addl	$4, %esp
+
 	ret
 
 sair:
